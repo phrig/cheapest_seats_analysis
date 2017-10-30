e571bbc305d63b510c11b597ba6099cc43d3736d
wisygig <jason.mcgibbon@gmail.com>
2017-10-29 21:02:38 -0400

    Refactor common operations.

--- etl.py
+++ etl.py
@@ -1,77 +1,102 @@
-from functools import wraps
+import functools
+import geopandas as gpd
 import numpy as np
 import os
 import pandas as pd
 import re
+import shapely
 
 base = 'https://data.pa.gov/api/views/'
 urls = {'filer': base + '53wp-ib3s/rows.csv?accessType=DOWNLOAD',
-        'expense': base + 'btk5-mx6k/rows.csv?accessType=DOWNLOAD',
+        'contributions':  base + 'wb79-wsa4/rows.csv?accessType=DOWNLOAD',
         'debt': base + '8bef-t5zg/rows.csv?accessType=DOWNLOAD',
-        'receipt': base + 'w5um-i7zu/rows.csv?accessType=DOWNLOAD',
-        'contributions':  base + 'wb79-wsa4/rows.csv?accessType=DOWNLOAD'}
+        'expense': base + 'btk5-mx6k/rows.csv?accessType=DOWNLOAD',
+        'receipt': base + 'w5um-i7zu/rows.csv?accessType=DOWNLOAD'}
 
 
 def local_loader(path):
     def wrapper(loader):
-        @wraps(loader)
+        @functools.wraps(loader)
         def f():
             if os.path.exists(path):
-                df = load_from_pickle(path)
+                print('Loading data frame from {}.'.format(path))
+                df = pd.read_pickle(path)
+                print('Data frame loaded.')
             else:
-                df = loader()
+                df = gpd.GeoDataFrame(loader())
+                print('Data frame loaded.')
                 print('Writing dataframe to pickle at {}'.format(path))
                 df.to_pickle(path)
-                print('Done.')
+                print('Done.\n')
             return df
         return f
     return wrapper
+
+
+def process_df(df, date=None, amount=None, suffix=None):
+    print('Processing data frame...')
+    df[date] = format_date(df[date])
+    df['amount'] = df[amount].apply(amount_to_float)
+    df['filer_id'] = df['Filer Identification Number']
+    df['address'] = format_address(df, suffix)
+    df['geometry'] = format_geometry(df, suffix)
+    return df
 
 
 @local_loader('data/debt.pkl')
 def load_debt():
     fname = 'Campaign_Finance_Disclosure_Debt_Data_Current_State.csv'
     df = load_from_url(fname, urls['debt'])
-
-    print('Processing data frame...')
-    df['Debt Accrual Date'] = format_date(df['Debt Accrual Date'])
-    df['amount'] = df['Debt Amount'].apply(amount_to_float)
-    df['filer_id'] = df['Filer Identification Number']
-    df = get_df_lat_long(df, 'Debt Reporting')
-    df['address'] = format_address(df, 'Debt Reporting')
-
-    print('Data frame loaded.')
-    return df
+    return process_df(df,
+                      date='Debt Accrual Date',
+                      amount='Debt Amount',
+                      suffix='Debt Reporting')
 
 
 @local_loader('data/receipt.pkl')
 def load_receipt():
     fname = 'Campaign_Finance_Disclosure_Receipt_Data_Current_State.csv'
     df = load_from_url(fname, urls['receipt'])
+    return process_df(df,
+                      date='Receipt Date',
+                      amount='Receipt Amount',
+                      suffix='Receipt')
 
-    print('Processing data frame...')
-    df['Receipt Date'] = format_date(df['Receipt Date'])
-    df['amount'] = df['Receipt Amount'].apply(amount_to_float)
-    df['filer_id'] = df['Filer Identification Number']
-    df = get_df_lat_long(df, 'Receipt')
-    df['address'] = format_address(df, 'Receipt')
 
-    print('Data frame loaded.')
-    return df
+@local_loader('data/expense.pkl')
+def load_expense():
+    fname = 'Campaign_Finance_Disclosure_Expense_Data_Current_State.csv'
+    df = load_from_url(fname, urls['expense'])
+    return process_df(df,
+                      date='Expense Date',
+                      amount='Expense Amount',
+                      suffix='Expense')
+
+
+@local_loader('data/contrib.pkl')
+def load_contributions(save=True):
+    fname = 'Campaign_Finance_Disclosure_Contributions_Data_2017_State.csv'
+    df = load_from_url(fname,
+                       urls['contributions'],
+                       dtype={'Employer Zip Code': str})
+    return process_df(df,
+                      date='Contribution Date',
+                      amount='Contribution Amount',
+                      suffix='Contributor')
 
 
 @local_loader('data/filer.pkl')
 def load_filer():
     fname = 'Campaign_Finance_Disclosure_Filer_Data_Current_State.csv'
-    df = load_from_url(fname, urls['filer'], dtype={'Phone Number': str})
+    df = load_from_url(fname,
+                       urls['filer'],
+                       dtype={'Phone Number': str})
 
     print('Processing data frame...')
     df['type'] = df['Filer Type'].apply(get_filer_type).astype('category')
     df['filer_id'] = df['Filer Identification Number']
-    df = get_df_lat_long(df, 'Filer')
     df['address'] = format_address(df, 'Filer')
-
-    print('Data frame loaded.')
+    df['geometry'] = format_geometry(df, 'Filer')
     return df
 
 
@@ -84,50 +109,6 @@ def get_filer_type(x):
         return 'lobbyist'
     else:
         return 'unknown'
-
-
-@local_loader('data/expense.pkl')
-def load_expense():
-    fname = 'Campaign_Finance_Disclosure_Expense_Data_Current_State.csv'
-
-    df = load_from_url(fname, urls['expense'])
-
-    print('Processing data frame...')
-    df['Expense Date'] = format_date(df['Expense Date'])
-    df['amount'] = df['Expense Amount'].apply(amount_to_float)
-    df['filer_id'] = df['Filer Identification Number']
-    df = get_df_lat_long(df, 'Expense')
-    df['address'] = format_address(df, 'Expense')
-
-    print('Data frame loaded.')
-    return df
-
-
-@local_loader('data/contrib.pkl')
-def load_contributions(save=True):
-    fname = 'Campaign_Finance_Disclosure_Contributions_Data_2017_State.csv'
-
-    df = load_from_url(fname,
-                       urls['contributions'],
-                       dtype={'Employer Zip Code': str})
-
-    print('Processing data frame...')
-    df['Contribution Date'] = format_date(df['Contribution Date'])
-    df['amount'] = df['Contribution Amount'].apply(amount_to_float)
-    df['filer_id'] = df['Filer Identification Number']
-    df = get_df_lat_long(df, 'Contributor')
-    df['address'] = format_address(df, 'Contributor')
-
-    print('Data frame loaded.')
-    return df
-
-
-def load_from_pickle(path):
-    """Load pre-computed dataframe."""
-    print('Loading data frame from {}.'.format(path))
-    df = pd.read_pickle(path)
-    print('Data frame loaded.')
-    return df
 
 
 def load_from_url(fname, url, **kwargs):
@@ -152,7 +133,6 @@ def format_address(df, prefix):
     return df.loc[:, start: end].apply(' '.join, axis=1)
 
 
-
 def amount_to_float(amount):
     """Converts a dollar amount to a float."""
     if type(amount) == float and np.isnan(amount):
@@ -165,8 +145,27 @@ def amount_to_float(amount):
         return np.nan
 
 
-def extract_lat_long(x):
-    """Extract lat and long from an address."""
+def format_geometry(df, prefix):
+    """Format a GeoSeries of coordinates."""
+    df = df.apply(get_coord, axis=1, prefix=prefix)
+    return gpd.GeoSeries(df, name='geometry')
+
+
+def get_coord(x, prefix=None):
+    """Get coordinates from a location."""
+    loc = x['{} Location 1'.format(prefix)]
+    lat, long = get_lat_long(loc)
+    if np.isnan(lat):
+        loc = x['{} Location 2'.format(prefix)]
+        lat, long = get_lat_long(loc)
+    if np.isnan(lat):
+        return None
+    else:
+        return shapely.geometry.Point([long, lat])
+
+
+def get_lat_long(x):
+    """Get latitude and longitude from a string."""
     pattern = re.compile(r'\((.*), (.*)\)')
     lat, long = np.nan, np.nan
     if type(x) == float and np.isnan(x):
@@ -176,17 +175,3 @@ def extract_lat_long(x):
         if g:
             lat, long = g.groups()
     return float(lat), float(long)
-
-
-def get_lat_long(x, string=None):
-    lat, long = extract_lat_long(x[string + ' Location 1'])
-    if np.isnan(lat):
-        lat, long = extract_lat_long(x[string + ' Location 2'])
-    return '{} {}'.format(lat, long)
-
-
-def get_df_lat_long(df, string):
-    new_pd = df.apply(get_lat_long, axis=1, string=string)
-    new_pd = new_pd.str.split(expand=True).astype(float)
-    df[['lat', 'long']] = new_pd.copy()
-    return df
